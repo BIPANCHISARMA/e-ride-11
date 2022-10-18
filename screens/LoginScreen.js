f6ed0cf7385daa30944fe665d15061b1f9723415
BIPANCHISARMA <95870329+BIPANCHISARMA@users.noreply.github.com>
2022-10-18 10:17:40 +0530

    Add files via upload

---
+++ screens/LoginScreen.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import {
+  View,
+  StyleSheet,
+  ImageBackground,
+  Image,
+  TextInput,
+  TouchableOpacity,
+  Text,
+  Alert,
+  KeyboardAvoidingView
+} from "react-native";
+import firebase from "firebase";
+
+const bgImage = require("../assets/background1.png");
+const appIcon = require("../assets/appIcon.png");
